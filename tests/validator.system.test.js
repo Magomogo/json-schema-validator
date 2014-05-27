2eff0ec116db9c196745694980f7d5db15a9d020
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-05-27 16:41:20 +0200

    Introduce Validator.simple() to avoid repeating the common bootstrap code everywhere

--- tests/validator.system.test.js
+++ tests/validator.system.test.js
@@ -1,65 +1,34 @@
 (function () {
     'use strict';
 
-    var request = require('request'),
-        Validator = require('../src/validator.js'),
-        assert = require('assert'),
+    var assert = require('assert'),
+        Validator = require('../src/validator.js') ;
 
-        schemaLoader = function (url, callback) {
-
-            console.log(url);
-
-            request({
-                url: url,
-                strictSSL: false,
-                json: true
-            }, function (error, response, body) {
-                if (error) {
-                    callback(error);
-                    return;
-                }
-
-                if (200 !== response.statusCode) {
-                    callback({url: url, code: response.statusCode});
-                    return;
-                }
-
-                callback(null, body);
-            });
-        };
-
-    describe('validator in wild', function () {
-
+    describe('validator in the wild', function () {
         it('loads all referenced remote schemas', function (done) {
-            var validator = new Validator('https://www.swiss-hotels.com/availabilities-filtered/schema');
+            this.timeout(8000);
 
-            this.timeout(10000);
+            Validator.simple(
+                'https://www.swiss-hotels.com/availabilities-filtered/schema',
 
-            validator.fetchSchemas(schemaLoader, function (err) {
-                if (err) {
-                    done(new Error(JSON.stringify(err)));
-                } else {
+                function (error) {
+                    assert.ifError(error);
                     done();
                 }
-            });
+            );
         });
 
         it('does actual validation', function (done) {
-            var validator = new Validator('http://json-schema.org/geo');
+            Validator.simple('http://json-schema.org/geo', function (error, v) {
+                assert.ifError(error);
 
-            validator.fetchSchemas(schemaLoader, function (err) {
-                if (err) {
-                    done(new Error(JSON.stringify(err)));
-                } else {
-                    assert(validator.validate({
-                        latitude: 53.0,
-                        longitude: 43.0
-                    }, 'http://json-schema.org/geo').valid);
+                assert(v.validate(
+                    {latitude: 53.0, longitude: 43.0},
+                    'http://json-schema.org/geo'
+                ).valid);
 
-                    done();
-                }
+                done();
             });
         });
     });
-
 }());
