d5942e82aeadc79a11fe16dd2aa4727f9169d1a8
Ivan Krechetov <ikr@ikr.su>
2017-06-05 13:55:02 +0200

    Lint the json-validate script

--- scripts/json-validate.js
+++ scripts/json-validate.js
@@ -4,6 +4,7 @@
     'use strict';
 
     if (process.argv.length !== 3) {
+        /* jshint multistr:true */
         process.stdout.write('\n\
 This script validates JSON from stdin, using schema parameter\n\
 Usage:\n\
@@ -13,11 +14,10 @@ Usage:\n\
         process.exit(1);
     }
 
-    var request = require('request'),
-        async = require('async'),
+    var async = require('async'),
         Validator = require('../src/validator.js'),
         typeId = process.argv[2],
-        schemaFileUrl = typeId.split("#")[0],
+        schemaFileUrl = typeId.split('#')[0],
 
         readStdin = function (callback) {
             var data = '';
@@ -35,7 +35,6 @@ Usage:\n\
         validator: function (callback) { Validator.simple(schemaFileUrl, callback); },
         json: readStdin
     }, function (err, results) {
-
         if (err) {
             console.log('ERROR:', err);
         }
@@ -59,6 +58,5 @@ Usage:\n\
         ].join(' '));
 
         process.exit(1);
-
     });
 }());
